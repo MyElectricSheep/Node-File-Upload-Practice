d6db19dd2d0bb1d544def44490aaa7f02eee28fd
Ben Paget <ben.paget@live.fr>
2021-07-25 23:49:16 +0200

    S3 Upload Functionality

--- index.js
+++ index.js
@@ -1,51 +1,19 @@
 const express = require("express");
-const fs = require("fs");
 const db = require("./database/client");
-const upload = require("./utils/imageUploader");
-const path = require("path");
+const { uploadS3 } = require("./utils/imageUploader");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + "/public"));
-
-// Option 1 (no db setup):
-// app.post('/upload-profile-pic', upload.single('profile_pic'), (req, res) => {
-//     const {file, fileValidationError} = req
-//     if (!file) {
-//       return res.status(400).send('Please upload a file'); // 400 Bad Request
-//     }
-//     if (fileValidationError) {
-//       return res.status(400).send(fileValidationError);
-//     }
-//     console.log(file)
-//     res.send(`<div>You have uploaded this image: <br/> <img src="/uploads/${req.file.filename}" width="500" /></div>`);
-//   })
-
-// app.post('/upload-cat-pics', upload.array('cat_pics'), (req, res) => {
-//   const { files, fileValidationError } = req
-//   if (!files || !files.length) {
-//     return res.status(400).send('Please upload some files');
-//   }
-//   if (fileValidationError) {
-//     return res.status(400).send(fileValidationError);
-//   }
-//   console.log(files)
-//   res.send(`<div>You have uploaded these images: <br/> ${files.map(file => `<img src="/uploads/${file.filename}" width="500" />`)}</div>`);
-// })
-
-// Option 2: Alternative with custom middlewares
 const multerValidation = (req, res, next) => {
-  const { url, file, files, fileValidationError } = req;
-  if (url === "/upload-profile-pic") {
-    if (!file) return res.status(400).send("Please upload a file");
-  }
-  if (url === "/upload-cat-pics") {
-    if (!files || !files.length)
-      return res.status(400).send("Please upload some files");
-  }
-  if (fileValidationError) return res.status(400).send(fileValidationError);
-  console.log({ data: req.file || req.files });
+  const { file, files } = req;
+  const data = file || files;
+
+  if (!data)
+    return res.status(400).send("Error. At least one file is required.");
+
+  req.data = data;
+
   next();
 };
 
@@ -55,53 +23,53 @@ const uploadToDatabase = async (req, res, next) => {
       rows: [insertedPic],
     } = await db.query(
       `INSERT INTO images (name, path) VALUES ($1, $2) RETURNING *`,
-      [file.filename, "/uploads"]
+      [file.key, file.location]
     );
     return insertedPic;
   };
 
-  const { url, file, files } = req;
+  const { data } = req;
 
-  if (url === "/upload-profile-pic") {
-    const insertedPic = await uploadPic(file);
-    req.insertedPic = insertedPic;
-    next();
-  }
-  if (url === "/upload-cat-pics") {
-    const insertedPics = files.map((file) => uploadPic(file));
+  if (Array.isArray(data)) {
+    const insertedPics = data.map((file) => uploadPic(file));
     req.insertedPics = await Promise.all(insertedPics);
+    next();
+  } else {
+    const insertedPic = await uploadPic(data);
+    req.insertedPic = insertedPic;
     next();
   }
 };
 
 app.post(
-  "/upload-profile-pic",
-  upload.single("profile_pic"),
+  "/upload-single",
+  uploadS3.single("single_pic"),
   multerValidation,
   uploadToDatabase,
   (req, res) => {
-    const { path, name } = req.insertedPic;
+    const { path } = req.insertedPic;
 
-    res.send(
-      `<div>You have uploaded this image: <br/> <img src="${path}/${name}" width="500" /></div>`
-    );
+    res.json({
+      message: "File uploaded successfully",
+      code: 200,
+      location: path,
+    });
   }
 );
 
 app.post(
-  "/upload-cat-pics",
-  upload.array("cat_pics"),
+  "/upload-multiple",
+  uploadS3.array("multiple_pics"),
   multerValidation,
   uploadToDatabase,
   (req, res) => {
     const { insertedPics } = req;
-    let html = "";
 
-    insertedPics.forEach(
-      ({ path, name }) => (html += `<img src="${path}/${name}" width="500" />`)
-    );
-
-    res.send(`<div>You have uploaded these images: <br/> ${html} </div>`);
+    res.json({
+      message: "Files uploaded successfully",
+      code: 200,
+      location: insertedPics,
+    });
   }
 );
 
@@ -109,24 +77,35 @@ app.get("/get-pics/:id?", async (req, res) => {
   const { id } = req.params;
   try {
     if (!id) {
-      let html = "";
-      const { rows: allPics } = await db.query("SELECT * FROM images");
-      allPics.forEach((pic) => {
-        html += `<li><a href="http://localhost:${port}/get-pics/${pic.pic_id}">${pic.name}</a></li>`;
-      });
-      if (!html)
-        return res.send("<h2>There is no uploaded pictures so far</h2>");
-      res.send(
-        `<div><h2>Here are all the uploaded pictures so far:</h2> <br/> <ul>${html}</ul> </div>`
+      const { rows: allPics } = await db.query(
+        "SELECT * FROM images ORDER BY pic_id DESC"
       );
+      if (!allPics.length)
+        return res.json({
+          message: "No pictures have been uploaded so far",
+          code: 400,
+        });
+
+      return res.json({
+        message: "All uploaded pictures",
+        code: 200,
+        pictures: allPics,
+      });
     } else {
       const {
         rows: [onePic],
       } = await db.query("SELECT * FROM images WHERE pic_id=$1", [id]);
-      console.log(onePic);
-      res.send(
-        `<div>Here is the picture you requested: <br/> <img src="http://localhost:${port}${onePic.path}/${onePic.name}" alt="" width="500"/></div>`
-      );
+      if (!onePic)
+        return res.json({
+          message: "No picture with this id",
+          code: 400,
+        });
+
+      return res.json({
+        message: "Uploaded picture",
+        code: 200,
+        picture: onePic,
+      });
     }
   } catch (e) {
     console.log(e.message);
@@ -135,19 +114,13 @@ app.get("/get-pics/:id?", async (req, res) => {
 });
 
 app.get("/reset", async (req, res) => {
-  const uploadsPath = path.resolve(__dirname, "public", "uploads");
   try {
-    const uploadsDir = await fs.promises.readdir(uploadsPath);
-    const deletedFiles = uploadsDir.map((file) => {
-      return fs.promises.unlink(path.resolve(uploadsPath, file));
-    });
-    await Promise.all(deletedFiles);
     await db.query(`DROP TABLE IF EXISTS images`);
     await db.query(`
     CREATE TABLE images (
       pic_id SERIAL PRIMARY KEY,
-      name varchar(255) NOT NULL,
-      path varchar(255) NOT NULL
+      name VARCHAR(255) NOT NULL,
+      path VARCHAR(255) NOT NULL
    ); 
     `);
     console.log("DB + Uploads folder reset successfully");
@@ -158,7 +131,7 @@ app.get("/reset", async (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.send("S3 File Uploader");
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- utils/imageUploader.js
+++ utils/imageUploader.js
@@ -1,43 +1,28 @@
 const multer = require("multer");
 const path = require("path");
+const aws = require("aws-sdk");
+const multerS3 = require("multer-s3");
 
-// const uploadFolder = path.join(process.cwd(), "public", "uploads");
+const { S3_BUCKET_NAME } = process.env;
 
-// console.log({ cwd: process.cwd() }); // { cwd: '/Users/.../file-upload' }
-// cwd is a method of the global object process, returns a string value which is the current working directory of the Node.js process.
+aws.config.region = "eu-west-1";
+const s3 = new aws.S3();
 
-// console.log({ dirname: __dirname }); // { dirname: '/Users/.../file-upload/utils' }
-// The directory name of the current script as a string value. __dirname is not actually a global but rather local to each module.
-
-const uploadFolder = path.resolve("public", "uploads");
-
-// Difference between path.join and path.resolve
-// https://elfi-y.medium.com/all-about-path-in-node-c7ea1cc64f93
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, uploadFolder);
+const s3Storage = multerS3({
+  s3: s3,
+  bucket: S3_BUCKET_NAME,
+  acl: "public-read",
+  metadata: (req, file, cb) => {
+    cb(null, { fieldName: file.fieldname });
   },
-  filename: (req, file, cb) => {
+  key: (req, file, cb) => {
     cb(
       null,
-      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}` // will give something like: profile_pic-1624546714574.png
-      // or something simpler like: `${Date.now()}-${file.originalname}` // will give something like: 1624546714574-myOriginalImage_name.jpg
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
     );
   },
 });
 
-// Option 1: check only the file extension (works, but not bulletproof)
-// const fileFilter = (req, file, cb) => {
-//   // Accept image file types only
-//   if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
-//     req.fileValidationError = "Only image files are allowed!";
-//     return cb(new Error("Only image files are allowed!"), false);
-//   }
-//   cb(null, true);
-// };
-
-// Option 2: check the mimetype and the file extension (better)
 const isPicture = ({ originalname, mimetype }) => {
   const allowedMimeTypes = [
     "image/png",
@@ -53,12 +38,11 @@ const isPicture = ({ originalname, mimetype }) => {
 
 const fileFilter = (req, file, cb) => {
   if (!isPicture(file)) {
-    req.fileValidationError = "Only image files are allowed!";
     return cb(new Error("Only image files are allowed!"), false);
   }
   cb(null, true);
 };
 
-const upload = multer({ storage, fileFilter });
+const uploadS3 = multer({ storage: s3Storage, fileFilter });
 
-module.exports = upload;
+module.exports = { uploadS3 };
